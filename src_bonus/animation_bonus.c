6a0b3a9b8ccdcd35416519150936154cb825018b
Adam Khobba <akhobba@e1r12p2.1337.ma>
2024-04-03 07:36:54 +0000

    fix path checker

--- src_bonus/animation_bonus.c
+++ src_bonus/animation_bonus.c
@@ -12,30 +12,28 @@
 
 #include "../include_bonus/so_long_bonus.h"
 
-int	ft_anim_enem(t_data *data)
-{
-	int		height;
-	int		width;
-	char	*s;
+int ft_anim_enem(t_data *data) {
+  int height;
+  int width;
+  char *s;
 
-	height = 64;
-	width = 64;
-	s = NULL;
-	if (data->frames < 20)
-		s = "./img/Grid_f.xpm";
-	else if (data->frames < 30)
-		s = "./img/Grid2_f.xpm";
-	else if (data->frames < 40)
-		s = "./img/Grid3_f.xpm";
-	else if (data->frames < 60)
-		s = "./img/Grid4_f.xpm";
-	else if (data->frames < 80)
-	{
-		s = "./img/Grid5_f.xpm";
-		if (data->frames + 1 == 80)
-			data->frames = 0;
-	}
-	data->grid = mlx_xpm_file_to_image(data->mlx, s, &width, &height);
-	data->frames++;
-	return (0);
+  height = 64;
+  width = 64;
+  s = NULL;
+  if (data->frames < 20)
+    s = "./textures/Grid_f.xpm";
+  else if (data->frames < 30)
+    s = "./textures/Grid2_f.xpm";
+  else if (data->frames < 40)
+    s = "./textures/Grid3_f.xpm";
+  else if (data->frames < 60)
+    s = "./textures/Grid4_f.xpm";
+  else if (data->frames < 80) {
+    s = "./textures/Grid5_f.xpm";
+    if (data->frames + 1 == 80)
+      data->frames = 0;
+  }
+  data->grid = mlx_xpm_file_to_image(data->mlx, s, &width, &height);
+  data->frames++;
+  return (0);
 }
--- src_bonus/error_bonus.c
+++ src_bonus/error_bonus.c
@@ -12,58 +12,53 @@
 
 #include "../include_bonus/so_long_bonus.h"
 
-void	ft_plus_fts(t_map *map)
-{
-	map = check_rec(map);
-	map = check_items(map);
-	map = flood_fill(map, '*');
-	ft_free_double_pointer(map->map, map->height);
+void ft_plus_fts(t_map *map) {
+  map = check_rec(map);
+  map = check_items(map);
+  map = flood_fill(map, '*');
+  ft_free_double_pointer(map->map, map->height);
 }
 
-void	ft_init(t_map *map)
-{
-	map->roaderror = 0;
-	map->wallerror = 0;
-	map->patherror = 0;
-	map->itemserror = 0;
-	map->recerror = 0;
+void ft_init(t_map *map) {
+  map->roaderror = 0;
+  map->wallerror = 0;
+  map->patherror = 0;
+  map->itemserror = 0;
+  map->recerror = 0;
 }
 
-int	ft_perror(char *path, t_map *map)
-{
-	map->roaderror = 0;
-	ft_init(map);
-	if (!check_map(path, map))
-	{
-		if (map->patherror)
-			ft_putstr("invalid path");
-		if (map->recerror)
-			ft_putstr("the map should a rectangle frome");
-		if (map->itemserror)
-			ft_putstr("invalid items in map");
-		if (map->wallerror)
-			ft_putstr("The map should be surrounded by walls");
-		if (map->roaderror)
-			ft_putstr("Player can't access to exit or a collectif");
-		exit(1);
-	}
-	return (1);
+int ft_perror(char *path, t_map *map) {
+  map->roaderror = 0;
+  ft_init(map);
+  if (!check_map(path, map)) {
+    ft_putstr("Error\n");
+    if (map->patherror)
+      ft_putstr("invalid path");
+    if (map->recerror)
+      ft_putstr("the map should a rectangle frome");
+    if (map->itemserror)
+      ft_putstr("invalid items in map");
+    if (map->wallerror)
+      ft_putstr("The map should be surrounded by walls");
+    if (map->roaderror)
+      ft_putstr("Player can't access to exit or a collectif");
+    exit(1);
+  }
+  return (1);
 }
 
-void	ft_invalid_image(t_data *data)
-{
-	if (data->player == NULL)
-	{
-		ft_putstr("invalid image");
-		exit(1);
-	}
+void ft_invalid_image(t_data *data) {
+  if (data->player == NULL) {
+    ft_putstr("Error\n");
+    ft_putstr("invalid image");
+    exit(1);
+  }
 }
 
-void	ft_invalid_image_p(t_data *data)
-{
-	if (data->img_ptr == NULL)
-	{
-		ft_putstr("invalid image");
-		exit(1);
-	}
+void ft_invalid_image_p(t_data *data) {
+  if (data->img_ptr == NULL) {
+    ft_putstr("Error\n");
+    ft_putstr("invalid image");
+    exit(1);
+  }
 }
