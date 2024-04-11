70bdc97a6afd8dfde3672e8eddd2eb7e44c90bfc
Adam Khobba <akhobba@e1r12p5.1337.ma>
2024-04-11 18:56:22 +0000

    norm

--- src_bonus/animation_bonus.c
+++ src_bonus/animation_bonus.c
@@ -12,29 +12,31 @@
 
 #include "../include_bonus/so_long_bonus.h"
 
-int ft_anim_enem(t_data *data) {
-  int height;
-  int width;
-  char *s;
+int	ft_anim_enem(t_data *data)
+{
+	int		height;
+	int		width;
+	char	*s;
 
-  height = 64;
-  width = 64;
-  s = NULL;
-  if (data->frames < 20 * 2)
-    s = "./textures/Grid_f.xpm";
-  else if (data->frames < 30 * 2)
-    s = "./textures/Grid2_f.xpm";
-  else if (data->frames < 40 * 2)
-    s = "./textures/Grid3_f.xpm";
-  else if (data->frames < 60 * 2)
-    s = "./textures/Grid4_f.xpm";
-  else if (data->frames < 80 * 2) {
-    s = "./textures/Grid5_f.xpm";
-    if (data->frames + 1 == 80 * 2)
-      data->frames = 0;
-  }
-  data->grid = mlx_xpm_file_to_image(data->mlx, s, &width, &height);
-  ft_invalid_image_grid(data);
-  data->frames++;
-  return (0);
+	height = 64;
+	width = 64;
+	s = NULL;
+	if (data->frames < 20 * 2)
+		s = "./textures/Grid_f.xpm";
+	else if (data->frames < 30 * 2)
+		s = "./textures/Grid2_f.xpm";
+	else if (data->frames < 40 * 2)
+		s = "./textures/Grid3_f.xpm";
+	else if (data->frames < 60 * 2)
+		s = "./textures/Grid4_f.xpm";
+	else if (data->frames < 80 * 2)
+	{
+		s = "./textures/Grid5_f.xpm";
+		if (data->frames + 1 == 80 * 2)
+			data->frames = 0;
+	}
+	data->grid = mlx_xpm_file_to_image(data->mlx, s, &width, &height);
+	ft_invalid_image_grid(data);
+	data->frames++;
+	return (0);
 }
--- src_bonus/strncmp_bonus.c
+++ src_bonus/strncmp_bonus.c
@@ -12,74 +12,87 @@
 
 #include "../include_bonus/so_long_bonus.h"
 
-int ft_strncmp(const char *s1, const char *s2, size_t n) {
-  size_t i;
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
 
-  i = 0;
-  while (i < n && (s1[i] != '\0' || s2[i] != '\0')) {
-    if (s1[i] != s2[i])
-      return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-    i++;
-  }
-  return (0);
+	i = 0;
+	while (i < n && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		if (s1[i] != s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return (0);
 }
 
-void ft_putstr_mlx(t_data *data) {
-  char *str;
+void	ft_putstr_mlx(t_data *data)
+{
+	char	*str;
 
-  str = ft_itoa(data->move_count);
-  mlx_string_put(data->mlx, data->win, 64, 64, 0x592690, str);
-  free(str);
+	str = ft_itoa(data->move_count);
+	mlx_string_put(data->mlx, data->win, 64, 64, 0x592690, str);
+	free(str);
 }
 
-void ft_int(int *a, int *b, int move) {
-  if (move == 1) {
-    *a = -1;
-    *b = 0;
-  }
-  if (move == 2) {
-    *a = +1;
-    *b = 0;
-  }
-  if (move == 3) {
-    *a = 0;
-    *b = +1;
-  }
-  if (move == 4) {
-    *a = 0;
-    *b = -1;
-  }
+void	ft_int(int *a, int *b, int move)
+{
+	if (move == 1)
+	{
+		*a = -1;
+		*b = 0;
+	}
+	if (move == 2)
+	{
+		*a = +1;
+		*b = 0;
+	}
+	if (move == 3)
+	{
+		*a = 0;
+		*b = +1;
+	}
+	if (move == 4)
+	{
+		*a = 0;
+		*b = -1;
+	}
 }
 
-void ft_chose(t_data *data, int move) {
-  int a;
-  int b;
+void	ft_chose(t_data *data, int move)
+{
+	int	a;
+	int	b;
 
-  a = 0;
-  b = 0;
-  ft_int(&a, &b, move);
-  if (data->map->map[data->map->p_pos_y + a][data->map->p_pos_x + b] == 'E' &&
-      data->map->collected == data->map->collectif_count) {
-    data->map->p_pos_y = data->map->p_pos_y + a;
-    data->map->p_pos_x = data->map->p_pos_x + b;
-    data->move_count++;
-    ft_putstr_mlx(data);
-    ft_close_win(data);
-  } else if (data->map->map[data->map->p_pos_y + a][data->map->p_pos_x + b] ==
-             'G') {
-    data->map->p_pos_y = data->map->p_pos_y + a;
-    data->map->p_pos_x = data->map->p_pos_x + b;
-    data->move_count++;
-    ft_putstr_mlx(data);
-    ft_close_lose(data);
-  }
+	a = 0;
+	b = 0;
+	ft_int(&a, &b, move);
+	if (data->map->map[data->map->p_pos_y + a][data->map->p_pos_x + b] == 'E'
+		&& data->map->collected == data->map->collectif_count)
+	{
+		data->map->p_pos_y = data->map->p_pos_y + a;
+		data->map->p_pos_x = data->map->p_pos_x + b;
+		data->move_count++;
+		ft_putstr_mlx(data);
+		ft_close_win(data);
+	}
+	else if (data->map->map[data->map->p_pos_y + a][data->map->p_pos_x
+		+ b] == 'G')
+	{
+		data->map->p_pos_y = data->map->p_pos_y + a;
+		data->map->p_pos_x = data->map->p_pos_x + b;
+		data->move_count++;
+		ft_putstr_mlx(data);
+		ft_close_lose(data);
+	}
 }
 
-void ft_invalid_image_grid(t_data *data) {
-
-  if (data->grid == NULL) {
-    ft_putstr("Error\n");
-    ft_putstr("invalid image");
-    exit(1);
-  }
+void	ft_invalid_image_grid(t_data *data)
+{
+	if (data->grid == NULL)
+	{
+		ft_putstr("Error\n");
+		ft_putstr("invalid image");
+		exit(1);
+	}
 }
