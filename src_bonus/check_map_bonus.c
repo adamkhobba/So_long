8c73d673d1d25be1822f326c3d581881e98d3b07
Adam Khobba <akhobba@e1r10p3.1337.ma>
2024-04-15 15:31:04 +0100

    add README.md

--- src_bonus/check_map_bonus.c
+++ src_bonus/check_map_bonus.c
@@ -12,98 +12,87 @@
 
 #include "../include_bonus/so_long_bonus.h"
 
-int	line_len(int fd)
-{
-	int		counter;
-	char	*s;
+int line_len(int fd) {
+  int counter;
+  char *s;
 
-	counter = 0;
-	s = get_next_line(fd);
-	if (!s)
-		return (0);
-	while (s)
-	{
-		free(s);
-		s = get_next_line(fd);
-		counter++;
-	}
-	return (counter);
+  counter = 0;
+  s = get_next_line(fd);
+  if (!s)
+    return (0);
+  while (s) {
+    free(s);
+    s = get_next_line(fd);
+    counter++;
+  }
+  return (counter);
 }
 
-void	ft_free_double_pointer(char **map, int line)
-{
-	int	i;
+void ft_free_double_pointer(char **map, int line) {
+  int i;
 
-	i = 0;
-	while (i < line)
-	{
-		free(map[i]);
-		i++;
-	}
-	free(map);
+  i = 0;
+  while (i < line) {
+    free(map[i]);
+    i++;
+  }
+  free(map);
 }
 
-int	ft_charchr(char *str, int c)
-{
-	int	i;
-	int	len;
+int ft_charchr(char *str, int c) {
+  int i;
+  int len;
 
-	i = 0;
-	len = 0;
-	while (str[i])
-	{
-		if (str[i] == c)
-			len++;
-		i++;
-	}
-	return (len);
+  i = 0;
+  len = 0;
+  while (str[i]) {
+    if (str[i] == c)
+      len++;
+    i++;
+  }
+  return (len);
 }
 
-char	**fill_line(int fd, int line)
-{
-	int		i;
-	char	**map;
+char **fill_line(int fd, int line) {
+  int i;
+  char **map;
 
-	map = (char **)malloc(sizeof(char *) * (line + 1));
-	if (!map)
-		return (0);
-	map[0] = get_next_line(fd);
-	if (!map[0])
-		return (NULL);
-	i = 1;
-	while (i < line + 1)
-	{
-		map[i] = get_next_line(fd);
-		i++;
-	}
-	return (map);
+  map = (char **)malloc(sizeof(char *) * (line + 1));
+  if (!map)
+    return (0);
+  map[0] = get_next_line(fd);
+  if (!map[0])
+    return (NULL);
+  i = 1;
+  while (i < line + 1) {
+    map[i] = get_next_line(fd);
+    i++;
+  }
+  return (map);
 }
 
-int	check_map(const char *path, t_map *map)
-{
-	if (!check_path(path))
-	{
-		map->patherror = 1;
-		return (0);
-	}
-	map->fd = open(path, O_RDWR);
-	if (map->fd < 0)
-		return (0);
-	map->height = line_len(map->fd);
-	close(map->fd);
-	map->fd = open(path, O_RDWR);
-	map->valid = 1;
-	map->map = fill_line(map->fd, map->height);
-	if (map->map == NULL)
-		return (0);
-	if (check_wall(map->map, map->height) == 0)
-	{
-		map->wallerror = 1;
-		return (0);
-	}
-	ft_plus_fts(map);
-	close(map->fd);
-	if (!map->valid)
-		return (0);
-	return (1);
+int check_map(const char *path, t_map *map) {
+  map->fd = open(path, O_RDWR);
+  if (map->fd < 0)
+    return (0);
+  if (!check_path(path)) {
+    map->patherror = 1;
+    return (0);
+  }
+  map->height = line_len(map->fd);
+  close(map->fd);
+  map->fd = open(path, O_RDWR);
+  map->valid = 1;
+  map->map = fill_line(map->fd, map->height);
+  if (map->map == NULL)
+    return (0);
+  if (check_wall(map->map, map->height) == 0) {
+    map->wallerror = 1;
+    return (0);
+  }
+  ft_plus_fts(map);
+  close(map->fd);
+  if (!map->valid)
+    return (0);
+  return (1);
 }
--- src_bonus/draw_map_bonus.c
+++ src_bonus/draw_map_bonus.c
@@ -12,96 +12,89 @@
 
 #include "../include_bonus/so_long_bonus.h"
 
-void	img_ptr(t_data *data)
-{
-	int	width;
-	int	height;
+void img_ptr(t_data *data) {
+  int width;
+  int height;
 
-	width = 64;
-	height = 64;
-	data->wall = mlx_xpm_file_to_image(data->mlx, "./textures/wall.xpm", &width,
-			&height);
-	data->floor = mlx_xpm_file_to_image(data->mlx, "./textures/floor.xpm",
-			&width, &height);
-	data->coin = mlx_xpm_file_to_image(data->mlx, "./textures/coin1.xpm",
-			&width, &height);
-	if (data->map->collectif_count != data->map->collected)
-		data->exit = mlx_xpm_file_to_image(data->mlx, "./textures/exit_f.xpm",
-				&width, &height);
-	else
-		data->exit = mlx_xpm_file_to_image(data->mlx, "./textures/odoor_f.xpm",
-				&width, &height);
+  width = 64;
+  height = 64;
+  data->wall =
+      mlx_xpm_file_to_image(data->mlx, "./textures/wall.xpm", &width, &height);
+  data->floor =
+      mlx_xpm_file_to_image(data->mlx, "./textures/floor.xpm", &width, &height);
+  data->coin =
+      mlx_xpm_file_to_image(data->mlx, "./textures/coin1.xpm", &width, &height);
+  if (data->map->collectif_count != data->map->collected)
+    data->exit = mlx_xpm_file_to_image(data->mlx, "./textures/exit_f.xpm",
+                                       &width, &height);
+  else
+    data->exit = mlx_xpm_file_to_image(data->mlx, "./textures/odoor_f.xpm",
+                                       &width, &height);
 }
 
-int	grid(t_data *data)
-{
-	ft_anim_enem(data);
-	data->y = 0;
-	while (data->y < data->map->height)
-	{
-		data->x = 0;
-		while (data->x < data->map->width)
-		{
-			if (data->map->map[data->y][data->x] == 'G')
-				mlx_put_image_to_window(data->mlx, data->win, data->grid,
-					data->x * 64, data->y * 64);
-			data->x++;
-		}
-		data->y++;
-	}
-	return (1);
+int grid(t_data *data) {
+  ft_anim_enem(data);
+  data->y = 0;
+  while (data->y < data->map->height) {
+    data->x = 0;
+    while (data->x < data->map->width) {
+      if (data->map->map[data->y][data->x] == 'G')
+        mlx_put_image_to_window(data->mlx, data->win, data->grid, data->x * 64,
+                                data->y * 64);
+      data->x++;
+    }
+    data->y++;
+  }
+  return (1);
 }
 
-int	put_player(t_data *data)
-{
-	int	width;
-	int	height;
+int put_player(t_data *data) {
+  int width;
+  int height;
 
-	width = 64;
-	height = 64;
-	if (data->view == 2)
-		data->player = mlx_xpm_file_to_image(data->mlx,
-				"./textures/Character.xpm", &width, &height);
-	else if (data->view == 1)
-		data->player = mlx_xpm_file_to_image(data->mlx,
-				"./textures/Character_top.xpm", &width, &height);
-	else if (data->view == 3)
-		data->player = mlx_xpm_file_to_image(data->mlx,
-				"./textures/Character_right2.xpm", &width, &height);
-	else if (data->view == 4)
-		data->player = mlx_xpm_file_to_image(data->mlx,
-				"./textures/Character_left.xpm", &width, &height);
-	ft_invalid_image(data);
-	mlx_put_image_to_window(data->mlx, data->win, data->player,
-		data->map->p_pos_x * PIXEL, data->map->p_pos_y * PIXEL);
-	mlx_hook(data->win, 2, 1L << 0, &move, data);
-	return (1);
+  width = 64;
+  height = 64;
+  if (data->view == 2)
+    data->player = mlx_xpm_file_to_image(data->mlx, "./textures/Character.xpm",
+                                         &width, &height);
+  else if (data->view == 1)
+    data->player = mlx_xpm_file_to_image(
+        data->mlx, "./textures/Character_top.xpm", &width, &height);
+  else if (data->view == 3)
+    data->player = mlx_xpm_file_to_image(
+        data->mlx, "./textures/Character_right2.xpm", &width, &height);
+  else if (data->view == 4)
+    data->player = mlx_xpm_file_to_image(
+        data->mlx, "./textures/Character_left.xpm", &width, &height);
+  ft_invalid_image(data);
+  mlx_put_image_to_window(data->mlx, data->win, data->player,
+                          data->map->p_pos_x * PIXEL,
+                          data->map->p_pos_y * PIXEL);
+  mlx_hook(data->win, 2, 1L << 0, &move, data);
+  return (1);
 }
 
-int	draw_map(t_data *data)
-{
-	data->y = -1;
-	while (++data->y < data->map->height)
-	{
-		data->x = -1;
-		while (data->x++ < data->map->width)
-		{
-			if (data->map->map[data->y][data->x] == '1')
-				data->img_ptr = data->wall;
-			if (data->map->map[data->y][data->x] == 'C')
-				data->img_ptr = data->coin;
-			if (data->map->map[data->y][data->x] == 'E')
-				data->img_ptr = data->exit;
-			if (data->map->map[data->y][data->x] == '0')
-				data->img_ptr = data->floor;
-			if (data->map->map[data->y][data->x] == 'P')
-				data->img_ptr = data->floor;
-			ft_invalid_image_p(data);
-			mlx_put_image_to_window(data->mlx, data->win, data->img_ptr, data->x
-				* 64, data->y * 64);
-		}
-	}
-	put_player(data);
-	mlx_loop_hook(data->mlx, grid, data);
-	return (1);
+int draw_map(t_data *data) {
+  data->y = -1;
+  while (++data->y < data->map->height) {
+    data->x = -1;
+    while (data->x++ < data->map->width) {
+      if (data->map->map[data->y][data->x] == '1')
+        data->img_ptr = data->wall;
+      if (data->map->map[data->y][data->x] == 'C')
+        data->img_ptr = data->coin;
+      if (data->map->map[data->y][data->x] == 'E')
+        data->img_ptr = data->exit;
+      if (data->map->map[data->y][data->x] == '0')
+        data->img_ptr = data->floor;
+      if (data->map->map[data->y][data->x] == 'P')
+        data->img_ptr = data->floor;
+      ft_invalid_image_p(data);
+      mlx_put_image_to_window(data->mlx, data->win, data->img_ptr, data->x * 64,
+                              data->y * 64);
+    }
+  }
+  put_player(data);
+  mlx_loop_hook(data->mlx, grid, data);
+  return (1);
 }
